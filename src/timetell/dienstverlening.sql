26ff0818ed127d5c1f6f1be8c9f10999068d18c9
Niels van Dijk <niels@differentstep.com>
2018-07-24 15:33:54 +0200

    Changed dienstverlening sql to work with schemaname variable.

--- src/timetell/dienstverlening.sql
+++ src/timetell/dienstverlening.sql
@@ -11,46 +11,45 @@ Purpose:
 /*
 remove the views first, in the right order
 */
-DROP VIEW IF EXISTS vw_timetell_ivdi;
-
-DROP VIEW IF EXISTS vw_tableau_act;
-DROP VIEW IF EXISTS vw_tableau_cust;
-DROP VIEW IF EXISTS vw_tableau_emp;
-DROP VIEW IF EXISTS vw_tableau_hrs;
-DROP VIEW IF EXISTS vw_tableau_org;
-DROP VIEW IF EXISTS vw_tableau_prj;
-DROP VIEW IF EXISTS vw_tableau_sys_prj_niv;
-DROP VIEW IF EXISTS vw_tableau_hulp_week;
+DROP VIEW IF EXISTS "{schemaname}"."vw_timetell_ivdi";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_act";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_cust";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_emp";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_hrs";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_org";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_prj";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_sys_prj_niv";
+DROP VIEW IF EXISTS "{schemaname}"."vw_tableau_hulp_week";
 --------------------------------------------------------------
 -- 1/x: vw_tableau_act
 --------------------------------------------------------------
-CREATE OR REPLACE VIEW vw_tableau_act AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_tableau_act" AS
 SELECT  act_id
 ,       name AS activiteit
-FROM    "ACT";
+FROM    "{schemaname}"."ACT";
 
 --------------------------------------------------------------
 -- 2/x: vw_tableau_cust
 --------------------------------------------------------------
-CREATE OR REPLACE VIEW vw_tableau_cust AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_tableau_cust" AS
 SELECT  cust_id
 ,       name AS opdrachtgever
-FROM    "CUST";
+FROM    "{schemaname}"."CUST";
 
 --------------------------------------------------------------
 -- 3/x: vw_tableau_emp
 --------------------------------------------------------------
-CREATE OR REPLACE VIEW vw_tableau_emp AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_tableau_emp" AS
 SELECT  emp_id
 ,       lastname AS mdw_achternaam
 ,       middlename AS mdw_tussenvoegsels
 ,       firstname AS mdw_voornaam
-FROM    "EMP";
+FROM    "{schemaname}"."EMP";
 
 --------------------------------------------------------------
 -- 4/x: vw_tableau_hrs
 --------------------------------------------------------------
-CREATE OR REPLACE VIEW vw_tableau_hrs AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_tableau_hrs" AS
 SELECT  act_id
 ,       cust_id
 ,       emp_id
@@ -58,20 +57,20 @@ SELECT  act_id
 ,       prj_id
 ,       date AS datum
 ,       hours AS uren
-FROM    "HRS";
+FROM    "{schemaname}"."HRS";
 
 --------------------------------------------------------------
 -- 5/x: vw_tableau_org
 --------------------------------------------------------------
-CREATE OR REPLACE VIEW vw_tableau_org AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_tableau_org" AS
 SELECT  org_id
 ,       name AS organisatie
-FROM    "ORG";
+FROM    "{schemaname}"."ORG";
 
 --------------------------------------------------------------
 -- 6/x: vw_tableau_prj
 --------------------------------------------------------------
-CREATE OR REPLACE VIEW vw_tableau_prj AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_tableau_prj" AS
 SELECT  prj_id
 ,       nr AS project_nummer
 ,       name AS project
@@ -86,34 +85,34 @@ SELECT  prj_id
         WHEN LEFT(nr,2) = 'In' AND SUBSTRING(nr, 3, 1) IN ('0','1','2','3','4','5','6','7','8','9') THEN 'In'
         ELSE '?' END project_type
 ,       parent_id -- is used in vw_timetell_ivdi
-FROM    "PRJ";
+FROM    "{schemaname}"."PRJ";
 
 --------------------------------------------------------------
 -- 7/x: vw_tableau_sys_prj_niv
 --------------------------------------------------------------
 --DROP VIEW vw_tableau_sys_prj_niv;
-CREATE VIEW vw_tableau_sys_prj_niv AS
+CREATE VIEW "{schemaname}"."vw_tableau_sys_prj_niv" AS
 SELECT  niv_id AS prj_id
 ,       niv::VARCHAR(20) AS project_niveau
 ,       niv0_name AS project_op_niveau_0
 ,       niv1_name AS project_op_niveau_1
 ,       niv0_id::VARCHAR(20) AS project_id_op_niveau_0
 ,       niv1_id::VARCHAR(20) AS project_id_op_niveau_1
-FROM    "SYS_PRJ_NIV";
+FROM    "{schemaname}"."SYS_PRJ_NIV";
 
 --------------------------------------------------------------
 -- 8/x: vw_tableau_sys_prj_niv
 --------------------------------------------------------------
-CREATE OR REPLACE VIEW vw_tableau_hulp_week AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_tableau_hulp_week" AS
 SELECT extract(year FROM date) jaar, extract(week FROM date) weeknummer, Max(date) datum
-FROM "HRS"
+FROM "{schemaname}"."HRS"
 GROUP BY extract(year FROM date), extract(week FROM date);
 
 --------------------------------------------------------------
 -- Combine x views into just one
 --------------------------------------------------------------
 
-CREATE OR REPLACE VIEW vw_timetell_ivdi AS
+CREATE OR REPLACE VIEW "{schemaname}"."vw_timetell_ivdi" AS
 -- part 1: hrs records
 SELECT  'hrs' AS bron
 ,       hrs.datum
@@ -156,20 +155,20 @@ SELECT  'hrs' AS bron
         END app_prj_detail
 ,       NULL::float norm_uren
 ,       current_timestamp AS datum_tijd_verversen_extract
-FROM    vw_tableau_hrs AS hrs       -- we start with the facts
-LEFT JOIN vw_tableau_act AS act     -- and then, all dimensions
+FROM    "{schemaname}"."vw_tableau_hrs" AS hrs       -- we start with the facts
+LEFT JOIN "{schemaname}"."vw_tableau_act" AS act     -- and then, all dimensions
 ON      hrs.act_id = act.act_id
-LEFT JOIN vw_tableau_cust AS cust
+LEFT JOIN "{schemaname}"."vw_tableau_cust" AS cust
 ON      hrs.cust_id = cust.cust_id
-LEFT JOIN vw_tableau_emp AS emp
+LEFT JOIN "{schemaname}"."vw_tableau_emp" AS emp
 ON      hrs.emp_id = emp.emp_id
-LEFT JOIN vw_tableau_org AS org
+LEFT JOIN "{schemaname}"."vw_tableau_org" AS org
 ON      hrs.org_id = org.org_id
-LEFT JOIN vw_tableau_prj AS prj
+LEFT JOIN "{schemaname}"."vw_tableau_prj" AS prj
 ON      hrs.prj_id = prj.prj_id
-LEFT JOIN vw_tableau_sys_prj_niv prj_niv
+LEFT JOIN "{schemaname}"."vw_tableau_sys_prj_niv" prj_niv
 ON      hrs.prj_id = prj_niv.prj_id
-LEFT JOIN vw_tableau_prj AS parent
+LEFT JOIN "{schemaname}"."vw_tableau_prj" AS parent
 ON      prj.parent_id = parent.prj_id
 WHERE   uren <> 0
 UNION ALL
@@ -199,9 +198,9 @@ SELECT  'emp_contract'
 ,       NULL AS app_prj_detail
 ,       hours
 ,       current_timestamp AS datum_tijd_verversen_extract
-FROM    "EMP_CONTRACT" a
-JOIN    vw_tableau_hulp_week b
+FROM    "{schemaname}"."EMP_CONTRACT" a
+JOIN    "{schemaname}"."vw_tableau_hulp_week" b
 ON      b.datum >= fromdate
 AND     b.datum <= todate
-LEFT JOIN vw_tableau_emp c
+LEFT JOIN "{schemaname}"."vw_tableau_emp" c
 ON      a.emp_id = c.emp_id
