b1eb241fb37c33220caee4984952dcdfcc101e8c
Niels van Dijk <niels@differentstep.com>
2018-08-20 14:43:56 +0200

    OP-256 Added new V3 psql views from Hans Kleijn.

--- src/timetell/datapunt.sql
+++ src/timetell/datapunt.sql
@@ -2211,7 +2211,7 @@ CREATE MATERIALIZED VIEW "{schemaname}"."v_timetell_projectenoverzicht" AS
                   ON "Left".prj_niv1_id = "Right".prj_id) a_1) a;
 
 /*
-Updated (new) implementation from Hans Kleijn
+V2 implementation from Hans Kleijn
 */
 CREATE MATERIALIZED VIEW "{schemaname}"."v_timetell_projectenoverzicht_V2" AS
 SELECT
@@ -2328,6 +2328,152 @@ UNION
                 ON pv.org_id=o.org_id and o.parent_id is not null and o.org_id > 12 AND o.org_id < 21
                group by pv.prj_id, o.org_id, o.name) vo2 on vo2.prj_id=b2.prj_id
                LEFT JOIN "{schemaname}"."CUST" cust2 ON c2.cust_id=cust2.cust_id) project
+LEFT JOIN
+               (select h.prj_id,
+               date as hrs_date,
+               hours as hrs_hours,
+               internalrate as hrs_internalrate,
+               rate as hrs_rate,
+               hoursrate as hrs_hoursrate,
+               hoursinternalrate as hrs_hoursinternalrate,
+               emp.name as emp_name,
+               vafd.name as org_name,
+               emp.empcat as emp_empcat,
+               vafd.name ::TEXT as "Medew. Verantw.",
+               act.name as act_name
+               FROM "{schemaname}"."HRS" h
+               JOIN "{schemaname}"."EMP" emp ON h.emp_id=emp.emp_id
+               JOIN (Select eo.emp_id, o.org_id, o.name from "{schemaname}"."EMP_ORG" eo JOIN "{schemaname}"."ORG" o ON eo.org_id=o.org_id
+                              and o.parent_id is not null
+                              where o.todate=to_date('2999-12-31', 'YYYY-MM-DD') and eo.type=0
+                              group by eo.emp_id, o.org_id, o.name ) vafd
+                on vafd.emp_id=h.emp_id
+               JOIN "{schemaname}"."ACT" act on h.act_id=act.act_id
+               ) uren
+               ON project.prj_prj_id=uren.prj_id;
+
+/*
+V3 implementation from Hans Kleijn
+*/
+DROP materialized VIEW "{schemaname}"."v_timetell_projectenoverzicht_3";
+
+CREATE MATERIALIZED VIEW "{schemaname}"."v_timetell_projectenoverzicht_3" AS
+select
+uren.hrs_date,
+uren.hrs_hours,
+uren.hrs_internalrate,
+uren.hrs_rate,
+uren.hrs_hoursrate,
+uren.hrs_hoursinternalrate,
+project.cust_name,
+uren.org_name,
+uren.emp_empcat,
+uren.emp_name,
+uren."Medew. Verantw.",
+uren.act_name,
+project.prj_niv,
+project."prj_niv_name" ,
+project."prj_niv0_name",
+project."prj_niv1_name",
+project."prj_niv2_name",
+project."Project Verantw.",
+project."Project Leader",
+project."prj_niv_id",
+project."prj_niv0_id" ,
+project."prj_niv1_id",
+project."prj_niv2_id"       ,
+project."prj_prj_id",
+project."prj_parent_id" ,
+project.fromdate,
+project.todate,
+project.prj_name,
+project.calc_hours,
+project.calc_costs,
+project."Project Nummer"
+FROM (
+               select 0 :: INTEGER as prj_niv,
+               a0.name as "prj_niv_name" ,
+               a0.name as "prj_niv0_name",
+               NULL ::TEXT as "prj_niv1_name",
+               NULL ::character varying as "prj_niv2_name",
+               NULL :: character varying(50) as "Project Verantw.",
+               NULL :: CHARACTER VARYING AS "Project Leader",
+               NULL :: CHARACTER VARYING AS cust_name,
+               a0.prj_id "prj_niv_id",
+               a0.prj_id as "prj_niv0_id" ,
+               NULL :: INTEGER as "prj_niv1_id"               ,
+               NULL :: INTEGER as "prj_niv2_id"               ,
+               a0.prj_id as "prj_prj_id"  ,
+               NULL :: INTEGER as "prj_parent_id" ,
+               a0.fromdate      AS fromdate,
+               a0.todate        AS todate,
+               a0.name as prj_name,
+               a0.calc_hours  AS calc_hours,
+               a0.calc_costs AS calc_costs,
+               a0.nr AS "Project Nummer"
+               from "{schemaname}"."PRJ" a0
+               where  a0.parent_id is null
+UNION
+               select 1 :: INTEGER as prj_niv,
+               b1.name  as "prj_niv_name" ,
+               a1.name as "prj_niv0_name",
+               (b1.nr || ' - ' || b1.name) :: character varying as "prj_niv1_name" ,
+               NULL ::TEXT as "prj_niv2_name",
+               vo1.basis_team_name ::character varying(50) as  "Project Verantw.",
+               e1.name AS "Project Leader" ,
+               cust1.name AS cust_name,
+               b1.prj_id as "prj_niv_id" ,
+               a1.prj_id as "prj_niv0_id" ,
+               b1.prj_id as "prj_niv1_id"               ,
+               NULL :: INTEGER as "prj_niv2_id"               ,
+               b1.prj_id as "prj_prj_id"  ,
+               b1.parent_id as "prj_parent_id" ,
+               b1.fromdate      AS fromdate,
+               b1.todate        AS todate,
+               b1.name as prj_name,
+               b1.calc_hours  AS calc_hours,
+               b1.calc_costs AS calc_costs,
+               b1.nr AS "Project Nummer"
+               from "{schemaname}"."PRJ" a1  JOIN "{schemaname}"."PRJ" b1 ON a1.prj_id=b1.parent_id and  a1.parent_id is null
+               LEFT JOIN (Select prj_id,emp_id from "{schemaname}"."PRJ_LINK" where prjleader=1 group by prj_id, emp_id ) prjpl ON  b1.prj_id=prjpl.prj_id
+               JOIN "{schemaname}"."EMP" e1 on e1.emp_id=prjpl.emp_id
+               LEFT JOIN (select lp.dim_id as prj_id, max(soi.item) as basis_team_name
+                                                                                                from  "{schemaname}"."LABEL_PRJ" lp
+                                                                                                LEFT JOIN  "{schemaname}"."SYS_OPT_ITM" as soi on lp.item_id= soi.item_id
+                                                                                                where to_char(lp.todate, 'YYYYMMDD')='29991231'
+                                                                                                group by lp.dim_id) vo1 on vo1.prj_id=b1.prj_id
+               LEFT JOIN "{schemaname}"."CUST" cust1 ON b1.cust_id=cust1.cust_id
+UNION
+               select 2 :: INTEGER as prj_niv,
+               c2.name as "prj_niv_name",
+               a2.name as " prj_niv0_name",
+               (b2.nr || ' - ' || b2.name)  as "prj_niv1_name" ,
+               (c2.nr || ' - ' || c2.name) :: character varying as "prj_niv2_name",
+               vo2.basis_team_name ::character varying(50) as "Project Verantw.",
+               e2.name AS "Project Leader"  ,
+               cust2.name AS cust_name,
+               c2.prj_id as "prj_niv_id" ,
+               a2.prj_id as "prj_niv0_id" ,
+               b2.prj_id as "prj_niv1_id"               ,
+               c2.prj_id as "prj_niv2_id"               ,
+               c2.prj_id as "prj_prj_id"   ,
+               c2.parent_id as "prj_parent_id" ,
+               c2.fromdate      AS fromdate,
+               c2.todate        AS todate,
+               c2.name as prj_name,
+               c2.calc_hours  AS calc_hours,
+               c2.calc_costs AS calc_costs,
+               c2.nr AS "Project Nummer"
+               from "{schemaname}"."PRJ" a2  JOIN "{schemaname}"."PRJ" b2 ON a2.prj_id=b2.parent_id and a2.parent_id is null
+     JOIN "{schemaname}"."PRJ" c2 ON b2.prj_id=c2.parent_id
+               LEFT JOIN (Select prj_id,emp_id from "{schemaname}"."PRJ_LINK" where prjleader=1 group by prj_id, emp_id ) prjpl2 ON  b2.prj_id=prjpl2.prj_id
+               JOIN "{schemaname}"."EMP" e2 on e2.emp_id=prjpl2.emp_id
+               LEFT JOIN (select lp.dim_id as prj_id, max(soi.item) as basis_team_name
+                                                                                                                from  "{schemaname}"."LABEL_PRJ" lp
+                                                                                                                LEFT JOIN  "{schemaname}"."SYS_OPT_ITM" as soi on lp.item_id= soi.item_id
+                                                                                                    where to_char(lp.todate, 'YYYYMMDD')='29991231'
+                                                                                                                group by lp.dim_id) vo2 on vo2.prj_id=b2.prj_id
+                            LEFT JOIN "{schemaname}"."CUST" cust2 ON c2.cust_id=cust2.cust_id) project
 LEFT JOIN
                (select h.prj_id,
                date as hrs_date,
