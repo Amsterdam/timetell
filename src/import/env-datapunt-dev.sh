a2557594559e951a60515d17d16753b9e7d1bfa8
Eelke Jager <info@lytrix.com>
2019-07-08 13:12:32 +0200

    Added more envs to test in development

--- src/import/env-datapunt-dev.sh
+++ src/import/env-datapunt-dev.sh
@@ -5,5 +5,10 @@ set -u
 set -x
 
 export DATABASE_NAME=timetell
+export DATABASE_USER=timetell
+export DATABASE_PASSWORD=timetell
+export DATABASE_HOST=localhost
+export DATABASE_PORT=5444
+
 export OBJECTSTORE_CONTAINER=uploads
 export SQL_QUERY=datapunt
--- src/import/env-dienstverlening-dev.sh
+++ src/import/env-dienstverlening-dev.sh
@@ -5,5 +5,10 @@ set -u
 set -x
 
 export DATABASE_NAME=timetelldienstverlening
+export DATABASE_USER=timetell
+export DATABASE_PASSWORD=timetell
+export DATABASE_HOST=localhost
+export DATABASE_PORT=5444
+
 export OBJECTSTORE_CONTAINER=uploadIVCLDI
 export SQL_QUERY=dienstverlening
