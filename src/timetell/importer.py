c01453b60c0abdb33f4661830a6d1392ccf2f15f
Jeffrey Klardie <j.klardie@amsterdam.nl>
2020-03-19 15:54:06 +0100

    Added missing VW_PLAN

--- src/timetell/importer.py
+++ src/timetell/importer.py
@@ -941,7 +941,7 @@ _CUSTOMER_SETTINGS = dict(
         tables=(
            'ACT', 'EMP', 'ORG',  'JOB', 'CUST', 'CUST_CONTACT', 'EMP_CONTRACT',
            'EMP_ORG', 'PRJ', 'PRJ_LINK', 'SYS_PRJ_NIV', 'HRS', 'SYS_OPT_ITM',
-           'VW_LABEL_PRJ'
+           'VW_LABEL_PRJ', 'VW_PLAN'
         ),
         sql=pkg_resources.resource_string(
             __name__, 'datapunt.sql'
